fd7a43deff81809ec054a487bdbc8bb2b6a6537f
mesketh <chorleyboy@gmail.com>
2023-01-03 12:40:21 +0800

    :sparkles: Find Max Profit TDCC #731.

---
+++ FindMaxProfit/src/FindMaxProfit.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+/**
+ * From The Daily Coding Problem #731 [Easy]:
+ *
+ * <pr>
+ * This problem was asked by Facebook.
+ * <p>
+ * Given a array of numbers representing the stock prices of a company in chronological order, write
+ * a function that calculates the maximum profit you could have made from buying and selling that
+ * stock once. You must buy before you can sell it.
+ * <p>
+ * For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5
+ * dollars and sell it at 10 dollars.
+ * </pr>
+ *
+ * @author mark hesketh
+ */
+public class FindMaxProfit {
+
+  private static int[] prices;
+
+  public static void main(String[] args) {
+
+    var numStocks = Optional.of(args)
+        .filter(a -> a.length > 0)
+        .map(a -> args[0])
+        .stream()
+        .mapToInt(Integer::parseInt)
+        .findFirst()
+        .orElse(5);
+
+    prices = stockPrices(numStocks);
+
+    System.out.printf("Max profit of prices (%s) = %d",
+        Arrays.stream(prices).boxed().map(String::valueOf).collect(Collectors.joining(",")),
+        new FindMaxProfit().calculateProfit(prices.length - 1, 0));
+  }
+
+  private int calculateProfit(int startPos, int maxProfit) {
+
+    if (startPos <= 0) {
+      return maxProfit;
+    }
+
+    if ((prices[startPos] - prices[startPos - 1]) > maxProfit) {
+      maxProfit = prices[startPos] - prices[startPos - 1];
+    }
+
+    // tail recursion by passing maxProfit
+    return calculateProfit(startPos - 1, maxProfit);
+  }
+
+  private static int[] stockPrices(int noOfStocks) {
+    Random stockGenerator = new Random();
+    return stockGenerator.ints(noOfStocks, 25, 150).toArray();
+  }
+
+}
